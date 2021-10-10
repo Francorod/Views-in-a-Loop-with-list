d40a7ed2320f1e4f6582a1bba9e49ad52c293c1f
Franco Rod <81878824+Francorod@users.noreply.github.com>
2021-10-09 21:57:00 -0400

    Initial Commit

---
+++ Views in a Loop/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Views in a Loop
+//
+//  Created by Franco Rodrigues on 10/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Views in a Loop/Views_in_a_LoopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Views_in_a_LoopApp.swift
+//  Views in a Loop
+//
+//  Created by Franco Rodrigues on 10/9/21.
+//
+
+import SwiftUI
+
+@main
+struct Views_in_a_LoopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
